844b08d890dd74e24c5adeab468ca36967b93da9
lopes-gui <89878469+lopes-gui@users.noreply.github.com>
2022-10-08 18:55:09 -0300

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,51 @@
+package StarbuzzPizza;
+
+
+public abstract class Pizza {
+    String description = "Unknown Beverage";
+    
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    //public abstract double cost();
+    double pepperoniCost;
+    double chesseCost;
+    double veggiecost;
+    public double cost(){
+        double condimentCost = (double) 0.0;
+        
+        if(hasPepperoni()){
+            condimentCost += pepperoniCost;
+        }
+        
+        if(haschesse()){
+            condimentCost += chesseCost;
+        }
+        
+        if(hasVeggie()){
+            condimentCost += veggiecost;
+        }
+        
+       
+        
+        return condimentCost;
+    }
+
+    private boolean hasPepperoni() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean haschesse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean hasVeggie() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+
+    
+}
